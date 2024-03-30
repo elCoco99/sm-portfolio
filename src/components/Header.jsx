c5889590625c4fe18f1d4d7ee061b2b456cb8a44
El Coco <145827756+elCoco99@users.noreply.github.com>
2024-03-30 18:24:44 -0500

    fix en responsive css del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,24 @@ import './header.css'
 
 export function Header() {
   return (
-    <nav id='header' className="nav-bar">
-      <div className="nav-title">
+    <nav id='header' className='nav-bar'>
+      <div className='nav-title'>
         <h1 translate='no'>Marwa Studio</h1>
       </div>
       <div className='nav-items'>
-        <ul>
-          <li><a href='#'>About</a></li>
-          <li><a href='#'>Recent Work</a></li>
-          <li><a href='#footer'>Contact</a></li>
-        </ul>
+        <div className='nav-items-cont'>
+          <ul>
+            <li>
+              <a href='#'>About</a>
+            </li>
+            <li>
+              <a href='#'>Recent Work</a>
+            </li>
+            <li>
+              <a href='#footer'>Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
